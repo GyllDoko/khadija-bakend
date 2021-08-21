477e938f300cc6c4b6287e67665bbd8246dc12f4
DOKO ALFA Gyll-christ <dokogyll@gmail.com>
2021-08-21 21:39:33 +0100

    modify admin'

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,6 @@
 from account.models import Account
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 
 # Register your models here.
 
@@ -8,4 +9,12 @@ class AccountAdmin(admin.ModelAdmin):
     list_display = ['user', 'phone_number', "is_delivery_man", "is_cook"]
 
 
-admin.site.register(Account, AccountAdmin)
+# admin.site.register(Account, AccountAdmin)
+admin.site.unregister(User)
+admin.site.unregister(Group)
+# default: "Django Administration"
+admin.site.site_header = 'Restaurant App Administration'
+# default: "Site administration"
+admin.site.index_title = 'Restaurant'
+admin.site.site_title = 'Restaurant App | Admin'
+admin.site.enable_nav_sidebar = False
