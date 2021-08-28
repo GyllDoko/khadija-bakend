a952432957af44250cb462f15b30a9dbb6e1941f
DOKO ALFA Gyll-christ <dokogyll@gmail.com>
2021-08-28 12:15:10 +0100

    added orders and their products view functions

--- order/urls.py
+++ order/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from order import views
 urlpatterns = [
     path("save_order/", views.save_order),
-    path("get_order_products/<str:order>/", views.get_order_products),
+    path("get_order_products/<str:order_id>/", views.get_order_products),
     path("get_order/<int:user>/", views.get_order),
 ]
--- order/views.py
+++ order/views.py
@@ -6,7 +6,7 @@ from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from order.serializers import Order, OrderSerializer
+from order.serializers import Order, OrderProductSerializer, OrderSerializer
 from product.serializers import Product
 from account.models import User, Account
 import random
@@ -30,10 +30,21 @@ def save_order(request):
 
         return JsonResponse(True, safe=False)
 
-@csrf_exempt
-def get_order(request, user):
-    pass
 
 @csrf_exempt
-def get_order_products(request, order):
-    pass
+def get_order(request, user):
+    if request.method == "GET":
+        account = Account.objects.get(pk=user)
+        orders = account.orders.all()
+        order_serializer = OrderSerializer(orders, many=True)
+        return JsonResponse(order_serializer.data, safe=False)
+
+
+@csrf_exempt
+def get_order_products(request, order_id):
+    if request.method == "GET":
+        order = Order.objects.get(ticket=order_id)
+        orders_products = order.orders_products.all()
+        orders_products_serializer = OrderProductSerializer(
+            orders_products, many=True)
+        return JsonResponse(orders_products_serializer.data, safe=False)
