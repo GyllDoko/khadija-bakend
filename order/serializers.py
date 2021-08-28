5df49a1e02321a8cc1e32e05dc2fe50baf067b45
DOKO ALFA Gyll-christ <dokogyll@gmail.com>
2021-08-28 15:18:13 +0100

    remove product attribte from order serializer

--- order/serializers.py
+++ order/serializers.py
@@ -4,12 +4,11 @@ from product.serializers import ProductSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(many=True)
 
     class Meta:
         model = Order
         fields = ["id", "timestamp", "status",
-                  "total_price", "product", "ticket"]
+                  "total_price", "ticket"]
 
 
 class OrderProductSerializer(serializers.ModelSerializer):
